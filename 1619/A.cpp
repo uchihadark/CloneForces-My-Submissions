ebe9190927bb957e8eac9b5fadb53f1168e396fd
CloneForcesBot <cfcloneforces@bot.com>
2022-05-21 14:52:40 +0000

    Added my codes from Codeforces

---
+++ 1619/A.cpp
@@ -0,0 +1,5 @@
+#1: OK [0 ms, 0 mb]: ok 10 token(s): yes count is 5, no count is 5
+#2: OK [0 ms, 0 mb]: ok 100 token(s): yes count is 41, no count is 59
+#3: OK [15 ms, 0 mb]: ok YES
+#4: OK [0 ms, 0 mb]: ok NO
+#5: OK [0 ms, 0 mb]: ok NO
