b0d291287b591f5c8d7431f782a3a9f5ace01e28
CloneForcesBot <cfcloneforces@bot.com>
2022-11-09 14:53:19 +0000

    Added my codes from Codeforces

---
+++ 1747/B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+const long long mod = 1e9 + 7;
+#define int long long
+#define pi pair<int,int>
+#define vi vector<int>
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define TIME cerr << "\n" << (float)clock() / CLOCKS_PER_SEC * 1000 << " ms" << endl;
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define mem(x,val) memset((x),(val),sizeof(x))
+#define inf      1e18
+#define endl    "\n"
+using namespace std;
+
+void Solve() {
+    int n;
+    cin >> n;
+    int ans;
+    if (n % 2 == 0) {
+        ans = n / 2;
+    } else {
+        ans = (n + 1) / 2;
+    }
+    int i = 1, j = 3 * n;
+    cout << ans << endl;
+    while (i < j) {
+        cout << i << " " << j << endl;
+        i += 3;
+        j -= 3;
+    }
+}
+
+int32_t main() {
+    FAST_IO
+    int TC = 1;
+    cin >> TC;
+    while (TC--) {
+        Solve();
+    }
+    TIME
+}
