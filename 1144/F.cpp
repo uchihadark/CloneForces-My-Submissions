f982e5d30021152b601bdec2ff9c9081e2f8d67b
CloneForcesBot <cfcloneforces@bot.com>
2023-04-23 14:53:04 +0000

    Added my codes from Codeforces

---
+++ 1144/F.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+const long long mod = 1e9 + 7;
+#define int long long
+#define pi pair<int,int>
+#define vi vector<int>
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define TIME cerr << "\n" << (float)clock() / CLOCKS_PER_SEC * 1000 << " ms" << endl;
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define mem(x,val) memset((x),(val),sizeof(x))
+#define inf      1e18
+#define endl    "\n"
+using namespace std;
+
+void Solve() {
+    int n,m;
+    cin>> n >> m;
+    vector<vector<int>>adj(n);
+    vector<pi> edges(m);
+    for(int i= 0;i<m;i++){
+        int u, v;
+        cin >>u >> v;
+        u--,v--;
+        edges[i] = {u,v};
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    vector<int> side(n, -1);
+    bool is_bipartite = true;
+    queue<int> q;
+    for (int st = 0; st < n; ++st) {
+        if (side[st] == -1) {
+            q.push(st);
+            side[st] = 0;
+            while (!q.empty()) {
+                int v = q.front();
+                q.pop();
+                for (int u : adj[v]) {
+                    if (side[u] == -1) {
+                        side[u] = side[v] ^ 1;
+                        q.push(u);
+                    } else {
+                        is_bipartite &= side[u] != side[v];
+                    }
+                }
+            }
+        }
+    }
+    if(!is_bipartite){
+        cout <<"NO\n";
+        return;
+    }
+    cout <<"YES\n";
+    for(auto &x : edges){
+        if(side[x.first] == 0){
+            cout << 1;
+        }else{
+            cout << 0;
+        }
+    }
+
+}
+
+int32_t main() {
+    FAST_IO
+    int TC = 1;
+    // cin >> TC;
+    while (TC--) {
+        Solve();
+    }
+    TIME
+}
